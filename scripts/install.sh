578c44753992209f59d57d640f6f91fe9ebc56b3
yuekcc <yuekcc@qq.com>
2026-04-11 23:05:16 +0800

    完善脚本

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-c3c build --trust=full -O2
+sh scripts/release.sh
 cp build/mp.exe /D/app/mp-agent
 cp libcurl-x64.dll /D/app/mp-agent
--- scripts/release.sh
+++ scripts/release.sh
@@ -2,4 +2,5 @@
 
 set -ex
 
+rm -rf build
 c3c build --trust=full -O2 -D RELEASE
